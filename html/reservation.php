9c1ce59fdae2843f0155340aaf14b8ed11a17cd5
Clickin <z1z0b4v@gmail.com>
2017-12-14 22:01:13 +0900

    before division commit

--- html/reservation.php
+++ html/reservation.php
@@ -8,16 +8,21 @@ if (empty($_SESSION)) {
     echo "<meta http-equiv='refresh' content='0;url=login.php'>";
 }
 $id = $_SESSION['user_id'];
-if (!empty($_GET)) {
+if (isset($_GET['movie_id'])) {
     $movie_uid = $_GET['movie_id'];
+    debug($movie_uid);
     $sql = "SELECT seat_uid FROM seats WHERE movie_uid = '$movie_uid' AND valid = 1";
-    $reserve_result = $con->query($sql);
+    $reserved_result = $con->query($sql);
     $seat_reserved = array();
-    $sql = "SELECT * FROM movies WHERE movie_uid = '$movie_uid'";
-    $movie_result = $con->query($sql)->fetch_assoc();
-    foreach ($reserved_result as $row) {
-        array_push($seat_reserved, $row['seat_uid']);
+    $sql = "SELECT * FROM movies WHERE movie_uid=$movie_uid";
+    $movie_result = $con->query($sql);
+    $movie_data = $movie_result->fetch_assoc();
+    if (!empty($reserved_result)) {
+        foreach ($reserved_result as $row) {
+            array_push($seat_reserved, $row['seat_uid']);
+        }
     }
+    
 }
 else {
     $sql = "SELECT * FROM movies";
@@ -26,23 +31,19 @@ else {
 }
 
 ?>
+
 <style>
 .pic-list{border: 2px solid gray;margin: 10px;display: inline-block;}
 </style>
 
-<link rel="stylesheet" href="./css/screen.csss">
+<link rel="stylesheet" href="./css/screen.css">
 <body>
 <div class="container">
 <div class="row">
     <?php if (empty($_GET)) {?>
         <div class="col-md-6">
         <h1>영화 목록</h1>
-        <div class="pic-list">
-            <?php foreach ($movie_result as $row) {
-                echo "<img src='./pic/" . $row['pic'] . "' width='180' height='270'>";
-            } ?>
-        </div>
-        <form method="get" action="./reserve.php" role="form">
+        <form method="get" action="./reservation.php" role="form">
         <table class="table table-hover">
             <thead>
                 <tr>
@@ -72,26 +73,29 @@ else {
     <?php } ?>
     <?php if(!empty($_GET)) { ?>
     <div class="col-md-6">
-        <h1> <?php echo $movie_result['movie_name']; ?><h1>
-        <img src="./pic/<?php echo $movie_result['pic']; ?>" style="width:300px;height:370px;border:1px;">
+        <h1> <?php echo $movie_data['movie_name']; ?></h1>
         <div class="screen">screen</div>
-        <table border = 1 width = 300px class="seat">
+        <table border= "1" class="seat">
         <?php
             $uid = 0;
             for ($i = 0; $i < 12; $i++) {
                 echo "<tr><td>" .$list[$i] . "</td>";
                 for ($j = 0; $j < 13; $j++) {
-                    if (in_array($uid, $seat_reserved)) {
+                    if (empty($seat_reserved)) {
+                        echo "<td onclick=add($uid)'>$num[$j]</td>";
+                    }
+                    else if (in_array($uid, $seat_reserved)) {
                         echo "<td style='background-color:gray;'>X</td>";
                     }
                     else {
-                        echo "<td onclick=add($uid) style='background-color:red;'>$num[$j]</td>";
+                        echo "<td onclick=add($uid)'>$num[$j]</td>";
                     }
                     $uid++;
                 }
 
             }
         ?>
+        </table>
     </div>
     <?php } ?>
     </div>
